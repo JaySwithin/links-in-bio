186501b8d7c0b22981de34482d2b7f940469fa75
Swithin Okyere <swithin@Swithins-MacBook-Pro.local>
2021-09-17 14:58:01 +0000

    Changed a little something

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Home from "./Home";
+
 function App() {
   return (
-    <div className="App">
-      
+    <div className="bg-gray-100">
+      <div className="App">
+        <Home />
+      </div>
     </div>
+    
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+	return (
+		<div className="">
+			<div className="p-8">
+				<div className="mt-5">
+					<img className="w-36 h-36 rounded-full mx-auto" src="swithin.jpeg" alt="" width="384" height="512" />
+				</div>
+			</div>
+		</div>
+	)
+}
