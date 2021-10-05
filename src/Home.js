2c8b1d0965988a15b589cbd70db24775a23e63be
Swithin Okyere <jayswithin@gmail.com>
2021-10-05 17:24:53 +0000

    Some change

--- src/Home.js
+++ src/Home.js
@@ -8,8 +8,14 @@ export default function Home() {
 					<img className="profile-pic w-36 h-36 rounded-lg mx-auto border-solid border-red-400" src="swithin.png" alt="" width="384" height="512" />
 				</div>
 				<div className="mt-2 text-center text-white text-lg">
-					<a href="https://instagram.com/swithinn_">@swithinn_</a>
-				</div>
+					<a href="https://instagram.com/swithinn_" target="_blank" rel="noopener noreferrer">@swithinn_</a>
+				</div> 
+			</div>
+
+			<div className="lg:ml-60 mt-8 lg:mr-60">
+				<button className="h1 w-full h-14 mb-6 font-semibold highlight border-2 border-white" type="button">Check Out My Portfolio</button>
+				<button className="h1 w-full h-14 mb-6 font-semibold highlight border-2 border-white" type="button">Buy Me A Coffee</button>
+				<button className="h1 w-full h-14 mb-6 font-semibold highlight border-2 border-white" type="button">Contact Me</button>
 			</div>
 		</div>
 	)
