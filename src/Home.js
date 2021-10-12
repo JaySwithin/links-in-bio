24ec6c606104312aad464a72de1493a51a2d91a9
Swithin Okyere <jayswithin@gmail.com>
2021-10-12 23:43:14 +0000

    Fixed icon sizes and border radius

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
 	return (
 		<div className="">
-			<div className="p-8">
+			<div className="p-8 mt-4">
 				<div className="mt-5">
 					<img className="profile-pic w-36 h-36 rounded-lg mx-auto border-solid border-red-400" src="swithin.png" alt="" width="384" height="512" />
 				</div>
@@ -12,21 +12,21 @@ export default function Home() {
 				</div> 
 			</div>
 
-			<div className="ml-5 mr-5 lg:ml-60 lg:mr-60 mt-8">
+			<div className="ml-5 mr-5 lg:ml-60 lg:mr-60 mt-4">
 				<a href="https://swithinokyere.netlify.app">
-					<button className="h1 w-full h-14 mb-6 font-semibold highlight border-2 border-white" type="button">Check Out My Portfolio</button>
+					<button className="h1 w-full h-14 mb-4 font-semibold highlight border-2 border-white" type="button">Check Out My Portfolio</button>
 				</a>
 				<a href="https://www.buymeacoffee.com/swithin">
-					<button className="h1 w-full h-14 mb-6 font-semibold highlight border-2 border-white" type="button">Buy Me A Coffee</button>
+					<button className="h1 w-full h-14 mb-4 font-semibold highlight border-2 border-white" type="button">Buy Me A Coffee</button>
 				</a>
 				<a href="mailto:jayswithin@gmail.com">
-					<button className="h1 w-full h-14 mb-6 font-semibold highlight border-2 border-white" type="button">Contact Me</button>
+					<button className="h1 w-full h-14 mb-4 font-semibold highlight border-2 border-white" type="button">Contact Me</button>
 				</a>
 			</div>
 
 			<div className="text-center mt-6">
-				<a className="btn btn-outline-primary mr-2" href="https://gmail.com"><i className="bx bxl-twitter text-4xl px-3"></i></a>
-				<a className="btn btn-outline-primary ml-2" href="https://github.com/JaySwithin"><i className="bx bxl-github text-4xl px-3"></i></a>
+				<a className="btn btn-outline-primary mr-2 rounded-md" href="https://twitter.com/Swithinnn" target="_blank" rel="noreferrer"><i className="bx bxl-twitter text-4xl px-3"></i></a>
+				<a className="btn btn-outline-primary ml-2 rounded-md" href="https://github.com/JaySwithin" target="_blank" rel="noreferrer"><i className="bx bxl-github text-4xl px-3"></i></a>
 			</div>
 		</div>
 	)
